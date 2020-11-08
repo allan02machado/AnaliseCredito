aea19f11fe55d1da4c1548d00d9961ba4a527648
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-11-07 22:19:18 -0300

    Este commit exibe resposta ao exercício

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AnaliseCredito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parcelas;
+            double valor;
+            double valorParcela;
+            double rendaMensal;
+            double parcelaMaxima;
+
+            
+            Console.WriteLine("Precisando de um empréstimo?");
+            Console.Write("Digite o valor que deseja: ");
+            valor = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de parcelas que deseja: ");
+            parcelas = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite sua renda mensal: ");
+            rendaMensal = Convert.ToDouble(Console.ReadLine());
+            
+            valorParcela = valor / parcelas;
+            parcelaMaxima = rendaMensal * 30 /100;
+
+            Console.WriteLine($"Valor das parcelas ficariam: {valorParcela:C}");
+            Console.WriteLine($"Valor da parcela máxima: {parcelaMaxima:C}");
+
+            if(valorParcela <= parcelaMaxima)
+            {
+                Console.WriteLine("\nParabéns, seu crédito foi aprovado!!");
+            }
+            else
+            {
+                Console.WriteLine("\nSeu crédito não foi aprovado");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # AnaliseCredito
 Resposta ao exercício "AnaliseCredito"
+
+
+**Enunciado**
+
+Um cliente quer solicitar um empréstimo. Receba o valor solicitado, a quantidade de parcelas desejada e a renda mensal comprovada. Só autorize empréstimos cuja parcela não ultrapasse 30% da renda (desconsidere os juros).
+
+**Como executar:**
+
+Abra o programa e digite:
+
+dotnet run
